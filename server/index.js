ade7db44b748300c64a923dd2a67634c9973e2e1
JGPico <minq_2@hotmail.com>
2025-08-08 14:44:20 -0600

    server tables for words

--- server/index.js
+++ server/index.js
@@ -5,22 +5,33 @@ const app = express();
 app.use(express.json());
 
 // routes
-app.get('/', async (req, res) => {
-    res.sendStatus(200);
+app.get('/allWords', async (req, res) => {
+    try {
+        const data = await pool.query('SELECT * FROM words')
+        res.status(200).send(data.rows)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
-app.post('/', async (req, res) => {
-    const { name, location } = req.body;
-    res.status(200).json({
-        message: `YOUR KEYS WERE ${name} and ${location}`,
-    })
+app.post('/newWord', async (req, res) => {
+    const { word, hiragana } = req.body;
+    try {
+        await pool.query('INSERT INTO words (word, hiragana) VALUES ($1, $2)', [word, hiragana])
+        res.status(200).send({ message: "Successfully added child" })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
     // const result = await pool.query('SELECT * FROM words WHERE word = $1', [word]);
     // res.json(result.rows);
 });
 
 app.get('/setup', async (req, res) => {
     try {
-        //await pool.query('CREATE TABLE words( id SERIAL PRIMARY KEY, word VARCHAR(255) NOT NULL)')
+        await pool.query('CREATE TABLE words( id SERIAL PRIMARY KEY, word VARCHAR(100), hiragana VARCHAR(100))')
+        res.status(200).send({ message: "Successfully added table" })
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal server error' });
